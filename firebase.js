15a78fb9f3f717c3971cc91fb3a055c363d8ba7a
Ashirvad Jena <>
2021-05-05 19:38:04 +0530

    firebase file added

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: "AIzaSyD2xiufMXkXorqkVA2zo5nG7P1vnHw8DZI",
+	authDomain: "dup-e0bc9.firebaseapp.com",
+	projectId: "dup-e0bc9",
+	storageBucket: "dup-e0bc9.appspot.com",
+	messagingSenderId: "528044279611",
+	appId: "1:528044279611:web:0aa7d4e51eab1f37e2591e",
+	measurementId: "G-Q96CVHM8JX",
+};
